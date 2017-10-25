666cbb6d68406842c7b9fc3121b4860b64d0b33e
Alexandre Perez <alexandrecperez@gmail.com>
2017-10-25 22:34:23 +0100

    Turing tape printer.

--- turing-machine/main.cpp
+++ turing-machine/main.cpp
@@ -1,31 +1,36 @@
+#include "common/string.hpp"
+#include "common/printer.hpp"
+
 #include "tape.hpp"
 #include "machine.hpp"
 
 using namespace meta::turing;
 
-template <int i> struct I {};
-
-using set_cursor_1 = meta::partial<set_cursor, I<1>>;
-using set_cursor_0 = meta::partial<set_cursor, I<0>>;
-
+//helper functions
+using set_cursor_1 = meta::partial<set_cursor, meta::chr<'1'>>;
+using set_cursor_0 = meta::partial<set_cursor, meta::chr<'0'>>;
 using is_nil = meta::partial<meta::is_same_l, meta::nil>;
 
-struct q1;
-struct q2;
-
-struct q1 {
-    using transitions = meta::list<
-        transition<is_nil, meta::list<set_cursor_1, rshift>, q2>>;
-};
-
-struct q2 {
-    using transitions = meta::list<
-        transition<is_nil, meta::list<set_cursor_0, rshift>, q1>>;
-};
-
-using test1 = eval<tape<>, q1, 10>;
-
-
 int main() {
+
+    //prints sequence 1010101010....
+    struct q1; struct q2;
+
+    struct q1 {
+        using transitions = meta::list<
+            transition<is_nil, meta::list<set_cursor_1, rshift>, q2>>;
+    };
+
+    struct q2 {
+        using transitions = meta::list<
+            transition<is_nil, meta::list<set_cursor_0, rshift>, q1>>;
+    };
+
+    using initial_tape = tape<>;
+    using final_tape = eval<initial_tape, q1, 50>;
+    tape_printer<final_tape>::print();
+
+    meta::newline_printer::print();
+
     return 0;
 }
--- turing-machine/tape.hpp
+++ turing-machine/tape.hpp
@@ -2,6 +2,7 @@
 
 #include "common/list.hpp"
 #include "common/lambda.hpp"
+#include "common/printer.hpp"
 
 namespace meta {
 namespace turing {
@@ -12,6 +13,8 @@ template <typename cursor = nil,
           typename default_cursor = nil>
 struct tape {
     using get = cursor;
+    using r = right;
+    using l = left;
 
     using lshift = tape<peek<left, default_cursor>,
                         pop<left>,
@@ -41,6 +44,21 @@ using lshift = lambda<_lshift>;
 template <typename t>
 using _rshift = typename t::rshift;
 using rshift = lambda<_rshift>;
+
+template <typename t>
+struct _rewind {
+    using result = typename _rewind<typename t::lshift>::result;
+};
+
+template <typename cursor, typename right>
+struct _rewind<tape<cursor, list<>, right>> {
+    using result = tape<cursor, list<>, right>;
+};
+
+CREATE_ALIAS(rewind);
+
+template <typename t>
+using tape_printer = printer<typename rewind<t>::lshift::r>;
 
 }
 }
